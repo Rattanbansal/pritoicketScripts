942f27f34d4fc88a13db09333cd8aa4a2a62c072
rattan bansal <rattan.intersot@gmail.com>
2024-11-02 19:10:08 +0530

    new changes

--- CSS_Commission/readcsv.sh
+++ CSS_Commission/readcsv.sh
@@ -4,6 +4,7 @@ i=0
 while IFS= read -r line; do
   # Print the line (each line represents one row in the single-column CSV)
   echo "$line"
+  mysql -uadmin -predhat dummy -e "select * from orders where vt_group_no = '$line'"
   ((i++))
 done < "/home/inteesoft-admin/Downloads/records.csv"
 
