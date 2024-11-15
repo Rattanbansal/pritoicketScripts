033d5fc404190447b0e96d249eccb09e7536827d
Aadikar <rattan.intersoft@gmail.com>
2024-11-15 17:06:15 +0530

    added new changes

--- EE_Commissions/UpdateAccountLevelCommission.sh
+++ EE_Commissions/UpdateAccountLevelCommission.sh
@@ -18,6 +18,7 @@ LOCAL_HOST="localhost"
 LOCAL_USER="admin"
 LOCAL_PASS="redhat"
 LOCAL_NAME="priopassdb"
+LOCAL_NAME_1="priopassdb"
 GETBACKUP=$1
 IMPORTDATATOHOST=$2
 
@@ -75,7 +76,7 @@ else
 fi
 
 
-time mysql -h"$LOCAL_HOST" -u"$LOCAL_USER" -p"$LOCAL_PASS" -D"$LOCAL_NAME" -e "select * from (SELECT d.ticket_id as product_id, d.hotel_id as distributor_id, d.commission as commission, tlc.hotel_prepaid_commission_percentage, tlc.is_hotel_prepaid_commission_percentage, tlc.commission_on_sale_price, tlc.hotel_commission_net_price, tlc.ticket_net_price FROM "$LOCAL_NAME".distributors d left join "$LOCAL_NAME".ticket_level_commission tlc on d.hotel_id = tlc.hotel_id and d.ticket_id = tlc.ticket_id and tlc.deleted = '0' and tlc.is_adjust_pricing = '1') as base where ticket_net_price is not NULL and (ABS(commission-hotel_prepaid_commission_percentage) > '0.01' or commission_on_sale_price != '1' or is_hotel_prepaid_commission_percentage != '1')" > tlc_level_mismatch.csv
+time mysql -h"$LOCAL_HOST" -u"$LOCAL_USER" -p"$LOCAL_PASS" -D"$LOCAL_NAME" -e "select * from (SELECT d.ticket_id as product_id, d.hotel_id as distributor_id, d.commission as commission, tlc.hotel_prepaid_commission_percentage, tlc.is_hotel_prepaid_commission_percentage, tlc.commission_on_sale_price, tlc.hotel_commission_net_price, tlc.ticket_net_price FROM "$LOCAL_NAME_1".distributors d left join "$LOCAL_NAME".ticket_level_commission tlc on d.hotel_id = tlc.hotel_id and d.ticket_id = tlc.ticket_id and tlc.deleted = '0' and tlc.is_adjust_pricing = '1') as base where ticket_net_price is not NULL and (ABS(commission-hotel_prepaid_commission_percentage) > '0.01' or commission_on_sale_price != '1' or is_hotel_prepaid_commission_percentage != '1')" > tlc_level_mismatch.csv
 
 
 
