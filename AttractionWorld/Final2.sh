c6711759e7c06a8c7e3615bcb02e9d37a0897295
Aadikar <rattan.intersoft@gmail.com>
2024-11-29 15:07:06 +0530

    new changes

--- AttractionWorld/Final2.sh
+++ AttractionWorld/Final2.sh
@@ -15,7 +15,7 @@ SOURCE_DB_HOST='production-primary-db-node-cluster.cluster-ck6w2al7sgpk.eu-west-
 SOURCE_DB_USER='pipeuser'
 SOURCE_DB_PASSWORD='d4fb46eccNRAL'
 SOURCE_DB_NAME='priopassdb'
-CatalogID='164794092806645'
+CatalogID='160277415344249'
 ResellerId='686'
 BATCH_SIZE=150
 
