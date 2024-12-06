1dff9cf5669a1d1e544fd92489b83f0f31e1ee69
rattan bansal <rattan.intersoft@gmail.com>
2024-12-06 21:54:56 +0530

    added tlt and oac

---
+++ bigquery_synching/FullTABLEOAC/finalFullTableOAC.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e  # Exit immediately if any command exits with a non-zero status
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+echo "-------------Script Starting So removing all Json Files-----------"
+rm -rf *.json
+
+echo "-----------Started Deleting all records for bigquery--------"
+gcloud config set project prioticket-reporting
+bq query --use_legacy_sql=False --format=prettyjson \
+"delete FROM prioticket-reporting.prio_test.own_account_commissions_synch where 1=1"
+
+
+DBHOST='163.47.214.30'
+DBUSER='datalook'
+DBPWD='datalook2024$$'
+DBDATABASE='priopassdb'
+PORT="3307"
+# DBHOST='production-primary-db-node-cluster.cluster-ck6w2al7sgpk.eu-west-1.rds.amazonaws.com'
+# DBUSER='pipeuser'
+# DBPWD='d4fb46eccNRAL'
+# DBDATABASE='priopassdb'
+# PORT="3306"
+UploadData=$1
+
+LIMIT=100000
+OFFSET=0
+
+if [[ $UploadData == 2 ]]; then
+
+  while :; do
+
+    records=$(mysql -h $DBHOST --user=$DBUSER --port=$PORT --password=$DBPWD $DBDATABASE -N -e "select count(*) from (select * from own_account_commissions where deleted = '0' limit $OFFSET, $LIMIT) as base;") || exit 1
+
+    echo "$records"
+
+    echo "limit $OFFSET, $LIMIT"
+
+    echo "select * from own_account_commissions where deleted = '0' limit $OFFSET, $LIMIT" | time mysqlsh --sql --json --uri $DBUSER@$DBHOST:$PORT -p$DBPWD --database=$DBDATABASE >> own_account_commissions.json || exit 1
+
+    jq 'select(.warning | not)' own_account_commissions.json >> own_account_commissions1.json
+
+    rm own_account_commissions.json
+
+    jq 'walk(if type == "number" then (. * 100 | round / 100) else . end)' own_account_commissions1.json > own_account_commissions2.json
+
+    rm own_account_commissions1.json
+
+    jq .rows[] own_account_commissions2.json  >> own_account_commissionsrows.json
+
+    rm own_account_commissions2.json
+
+    cat own_account_commissionsrows.json | jq -c '.' >> own_account_commissions_ndnewjson.json
+
+    rm own_account_commissionsrows.json
+
+
+    bq load --source_format=NEWLINE_DELIMITED_JSON  prio_test.own_account_commissions_synch own_account_commissions_ndnewjson.json || exit 1
+
+    # exit
+
+    rm own_account_commissions_ndnewjson.json
+
+
+    OFFSET=$(($OFFSET + $LIMIT))
+
+    if [[ $records < $LIMIT ]]; then
+      echo "No more records to fetch. Exiting loop."
+      break
+    fi
+  done
+
+fi
+
+  echo "Rattan"
+
+  bq query --use_legacy_sql=False --format=prettyjson \
+  "with oact as (select *,row_number() over(partition by id order by last_modified_at desc ) as rn from prio_test.own_account_commissions), oacl as (select *,row_number() over(partition by id order by last_modified_at desc ) as rn from prio_olap.own_account_commissions), oactrn as (select * from oact where rn = 1), oaclrn as (select * from oacl where rn = 1), final as (select oactrn.*, oaclrn.id as ids from oactrn left join oaclrn on oactrn.id = oaclrn.id and (oactrn.last_modified_at = oaclrn.last_modified_at or oactrn.last_modified_at < oaclrn.last_modified_at)) select *except(rn,ids) from final where ids is NULL" > mismatch.json
---
+++ bigquery_synching/FullTABLETLT/finalFullTableTLT.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -e  # Exit immediately if any command exits with a non-zero status
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+echo "-------------Script Starting So removing all Json Files-----------"
+rm -rf *.json
+
+echo "-----------Started Deleting all records for bigquery--------"
+gcloud config set project prioticket-reporting
+bq query --use_legacy_sql=False --format=prettyjson \
+"delete FROM prioticket-reporting.prio_test.template_level_tickets_synch where 1=1"
+
+
+# DBHOST='163.47.214.30'
+# DBUSER='datalook'
+# DBPWD='datalook2024$$'
+# DBDATABASE='priopassdb'
+# PORT="3307"
+DBHOST='production-primary-db-node-cluster.cluster-ck6w2al7sgpk.eu-west-1.rds.amazonaws.com'
+DBUSER='pipeuser'
+DBPWD='d4fb46eccNRAL'
+DBDATABASE='priopassdb'
+PORT="3306"
+UploadData=$1
+
+LIMIT=150000
+OFFSET=0
+
+if [[ $UploadData == 2 ]]; then
+
+  while :; do
+
+    records=$(mysql -h $DBHOST --user=$DBUSER --port=$PORT --password=$DBPWD $DBDATABASE -N -e "select count(*) from (select * from template_level_tickets where deleted = '0' limit $OFFSET, $LIMIT) as base;") || exit 1
+
+    echo "$records"
+
+    echo "limit $OFFSET, $LIMIT"
+
+    echo "select * from template_level_tickets where deleted = '0' limit $OFFSET, $LIMIT" | time mysqlsh --sql --json --uri $DBUSER@$DBHOST:$PORT -p$DBPWD --database=$DBDATABASE >> template_level_tickets.json || exit 1
+
+    jq 'select(.warning | not)' template_level_tickets.json >> template_level_tickets1.json
+
+    rm template_level_tickets.json
+
+    jq 'walk(if type == "number" then (. * 100 | round / 100) else . end)' template_level_tickets1.json > template_level_tickets2.json
+
+    rm template_level_tickets1.json
+
+    jq .rows[] template_level_tickets2.json  >> template_level_ticketsrows.json
+
+    rm template_level_tickets2.json
+
+    cat template_level_ticketsrows.json | jq -c '.' >> template_level_tickets_ndnewjson.json
+
+    rm template_level_ticketsrows.json
+
+
+    bq load --source_format=NEWLINE_DELIMITED_JSON  prio_test.template_level_tickets_synch template_level_tickets_ndnewjson.json || exit 1
+
+    # exit
+
+    rm template_level_tickets_ndnewjson.json
+
+
+    OFFSET=$(($OFFSET + $LIMIT))
+
+    if [[ $records < $LIMIT ]]; then
+      echo "No more records to fetch. Exiting loop."
+      break
+    fi
+  done
+
+else
+
+  bq query --use_legacy_sql=False --format=prettyjson \
+  "with tltt as (select *,row_number() over(partition by template_level_tickets_id order by last_modified_at desc ) as rn from prio_test.template_level_tickets), tltl as (select *,row_number() over(partition by template_level_tickets_id order by last_modified_at desc ) as rn from prio_olap.template_level_tickets), tlttrn as (select * from tltt where rn = 1), tltlrn as (select * from tltl where rn = 1), final as (select tlttrn.*, tltlrn.template_level_tickets_id as ids from tlttrn left join tltlrn on tlttrn.template_level_tickets_id = tltlrn.template_level_tickets_id and (tlttrn.last_modified_at = tltlrn.last_modified_at or tlttrn.last_modified_at < tltlrn.last_modified_at)) select template_level_tickets_id, template_id, ticket_id, is_pos_list, is_suspended, created_at, market_merchant_id, content_description_setting, last_modified_at, catalog_id, merchant_admin_id, publish_catalog, product_verify_status, deleted from final where ids is NULL" > mismatch.json
+
+fi
