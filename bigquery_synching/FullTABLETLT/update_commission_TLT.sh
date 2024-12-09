4c5142d10e7f42971461fef9f3f0a6ad35280e67
Aadikar <rattan.intersoft@gmail.com>
2024-12-09 12:08:35 +0530

    new changes

--- bigquery_synching/FullTABLETLT/update_commission_TLT.sh
+++ bigquery_synching/FullTABLETLT/update_commission_TLT.sh
@@ -42,3 +42,6 @@ for (( i=0; i<$total_ids; i+=$BATCH_SIZE )); do
     # Execute the query
     # mysql -u "$DBUSER" -h "$DBHOST" --port=$PORT -p"$DBPWD" "$DBDATABASE" -e "$query"
 done
+
+# bq query --use_legacy_sql=False --max_rows=1000000 --format=prettyjson \
+# "insert into prioticket-reporting.prio_olap.template_level_tickets (with tltt as (select *,row_number() over(partition by template_level_tickets_id order by last_modified_at desc ) as rn from prio_test.template_level_tickets), tltl as (select *,row_number() over(partition by template_level_tickets_id order by last_modified_at desc ) as rn from prio_olap.template_level_tickets), tlttrn as (select * from tltt where rn = 1), tltlrn as (select * from tltl where rn = 1), final as (select tlttrn.*, tltlrn.template_level_tickets_id as ids from tlttrn left join tltlrn on tlttrn.template_level_tickets_id = tltlrn.template_level_tickets_id and (tlttrn.last_modified_at = tltlrn.last_modified_at or tlttrn.last_modified_at < tltlrn.last_modified_at)) select template_level_tickets_id,template_id,ticket_id,is_pos_list,created_at,market_merchant_id,content_description_setting,last_modified_at,catalog_id,merchant_admin_id,publish_catalog,deleted,hostname,is_suspended,product_verify_status from final where ids is NULL)" || exit 1
