b43462ddbd6193766512a2fcaa61e30eb40d811a
Aadikar <rattan.intersoft@gmail.com>
2024-12-18 15:43:12 +0530

    added compare data script

---
+++ BigqueryMismatch/CompareData.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Mismatch for QR_codes
+
+tablename=qr_codes
+days=$1
+partitioncolumn=cod_id
+
+rm -f *.json
+
+# Check if the required arguments are provided
+if [ -z "$1" ]; then
+    echo "Error: All arguments are required."
+    echo "Usage: $0 noofDays"
+    exit 1
+fi
+
+source fetchdata.sh $tablename $days $tablename.json $partitioncolumn
+
+python compare_record.py $tablename.json $partitioncolumn $tablename-Diff.json
+
+tablename=modeventcontent
+partitioncolumn=mec_id
+
+source fetchdata.sh $tablename $days $tablename.json $partitioncolumn
+
+python compare_record.py $tablename.json $partitioncolumn $tablename-Diff.json
+
+tablename=ticketpriceschedule
+partitioncolumn=id
+
+source fetchdata.sh $tablename $days $tablename.json $partitioncolumn
+
+python compare_record.py $tablename.json $partitioncolumn $tablename-Diff.json
