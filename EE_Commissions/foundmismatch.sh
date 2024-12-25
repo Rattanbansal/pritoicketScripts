959541ee3ed595b95b289f36d508ae9bb378210f
Aadikar <rattan.intersoft@gmail.com>
2024-12-25 15:13:33 +0530

    Added Queries Related to EE Commission update and also to check which data client not provided

--- EE_Commissions/foundmismatch.sh
+++ EE_Commissions/foundmismatch.sh
@@ -16,6 +16,8 @@ echo "" > Catalog_linking_still_not_provided_in_matrix_Sheet.csv
 echo "" > Catalog_wrong_Linked.csv
 echo "" > Product_id_Sold_But_not_In_matrix.csv
 echo "" > Product_id_Sold_But_not_In_matrixReseller.csv
+echo "" > distributor_id_Sold_But_not_In_matrix.csv
+echo "" > reseller_id_Sold_But_not_In_matrixreseller.csv
 
 if [[ $Insertdata == 2 ]]; then
 
@@ -62,7 +64,13 @@ mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -D $DB_NAME -e "select * from (select *
 
 read -r user_input
 
-# select * from (select * from (select DISTINCT(ticket_id) as ordered_product from bigqueryData where reseller_id = '541') as orderData left join (select distinct(ticket_id) as ticket_id from distributors) d on orderData.ordered_product = d.ticket_id) as final where ticket_id is NULL; 
+mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -D $DB_NAME -e "select * from (select * from (select DISTINCT(hotel_id) as order_distributor from bigqueryData where reseller_id = '541') as orderData left join (select distinct(hotel_id) as hotel_id from distributors) d on orderData.order_distributor = d.hotel_id) as final where hotel_id is NULL limit 100;" >> distributor_id_Sold_But_not_In_matrix.csv ## product oid sold but these product id commission not provided in the sheet of Matrix
+
+read -r user_input
+
+mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -D $DB_NAME -e "select * from (select * from (select DISTINCT(reseller_id) as order_reseller_id from bigqueryData where reseller_id != '541') as orderData left join (select distinct(reseller_id) as reseller_id from pricelist) d on orderData.order_reseller_id = d.reseller_id) as final where reseller_id is NULL limit 100;" >> reseller_id_Sold_But_not_In_matrixreseller.csv ## product oid sold but these product id commission not provided in the sheet of Matrix
+
+read -r user_input
 
 # mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -D $DB_NAME -e "with qr_codess as (select reseller_id, channel_id from priopassdb.qr_codes where cashier_type = '1' and channel_id is not NULL group by reseller_id, channel_id), channels as (select d.*, qc.reseller_id as qc_reseller_id, qc.channel_id from rattan.pricelist d left join qr_codess qc on d.reseller_id = qc.reseller_id), final as (select c.*, clc.ticketpriceschedule_id, clc.resale_currency_level, clc.currency, clc.commission_on_sale_price, clc.is_hotel_prepaid_commission_percentage, clc.hotel_prepaid_commission_percentage, clc.ticket_net_price, clc.hotel_commission_net_price, (clc.ticket_net_price*c.commission/100) as hotel_commission_should_be from channels c left join priopassdb.channel_level_commission clc on c.channel_id = clc.channel_id and c.ticket_id = clc.ticket_id and clc.deleted = '0' and clc.is_adjust_pricing = '1') select *, ABS(hotel_commission_net_price - hotel_commission_should_be) as gap from final where ABS(hotel_commission_net_price - hotel_commission_should_be) > '0.05' or commission_on_sale_price != '1' or is_hotel_prepaid_commission_percentage != '1' or ticketpriceschedule_id is NULL;" >> channel_level_mismatch.csv
 
