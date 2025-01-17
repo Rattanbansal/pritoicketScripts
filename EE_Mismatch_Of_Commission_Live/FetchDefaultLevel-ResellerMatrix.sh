c2b7accef04a051fcff642bfe24658e9d7c8dca4
Aadikar <rattan.intersoft@gmail.com>
2025-01-17 18:22:36 +0530

    new changes

---
+++ EE_Mismatch_Of_Commission_Live/FetchDefaultLevel-ResellerMatrix.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e  # Exit immediately if any command exits with a non-zero status
+TIMEOUT_PERIOD=15
+DB_NAME="priopassdb"
+outputFile="$PWD/records/reseller-Matrix_DefaultLevel_mismatch.csv"
+source ~/vault/vault_fetch_creds.sh
+
+mkdir -p $PWD/records
+# Fetch credentials for 20Server
+fetch_db_credentials "PrioticketLivePriomaryroPipe"
+
+## GEt Distinct Reseller_id from Pricelist table
+
+echo "channel_level_commission_id,product_id,admin_id,commission,hotel_prepaid_commission_percentage,is_hotel_prepaid_commission_percentage,commission_on_sale_price,hgs_prepaid_commission_percentage,ticket_net_price,museum_net_commission,merchant_net_commission,hotel_commission_net_price,hgs_commission_net_price,resale_currency_level,currency" > $outputFile
+
+reseller_ids=$(timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -sN  -e "select distinct reseller_id from pricelist where reseller_id not in (541,671)") || exit 1
+
+for reseller_id in ${reseller_ids}
+
+do
+
+    echo "Fetching Data for reseller_id :: $reseller_id"
+    
+    channel_ids=$(timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -sN -e "select distinct channel_id from priopassdb.qr_codes where cashier_type = '1' and channel_id > '0' and channel_id is not NULL and reseller_id = '$reseller_id'")
+
+    for channel_id in ${channel_ids}
+
+    do 
+        echo "-------------Channel Id Processing: $channel_id---------"
+        timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -sN -e "with qr_codess as (select reseller_id, channel_id from priopassdb.qr_codes where cashier_type = '1' and channel_id = '$channel_id' and channel_id is not NULL group by reseller_id, channel_id), channels as (select d.*, qc.reseller_id as qc_reseller_id, qc.channel_id from priopassdb.pricelist d join qr_codess qc on d.reseller_id = qc.reseller_id), final as (select tlc.channel_level_commission_id,d.ticket_id as product_id, d.reseller_id as admin_id, d.commission as commission, tlc.hotel_prepaid_commission_percentage, tlc.is_hotel_prepaid_commission_percentage, tlc.commission_on_sale_price, tlc.hgs_prepaid_commission_percentage, tlc.ticket_net_price, tlc.museum_net_commission, tlc.merchant_net_commission, tlc.hotel_commission_net_price, tlc.hgs_commission_net_price, tlc.resale_currency_level, tlc.currency from channels d left join priopassdb.channel_level_commission tlc on d.channel_id = tlc.channel_id and d.ticket_id = tlc.ticket_id and tlc.deleted = '0' and tlc.is_adjust_pricing = '1') select * from final where ticket_net_price is not NULL and (ABS(commission-hotel_prepaid_commission_percentage) > '0.01' or commission_on_sale_price != '1' or is_hotel_prepaid_commission_percentage != '1' or ABS(ticket_net_price-museum_net_commission-merchant_net_commission-hotel_commission_net_price-hgs_commission_net_price) > '0.02' or ABS(ticket_net_price-museum_net_commission-hotel_commission_net_price) > '0.02');" >> $outputFile
+
+        sleep 3
+    done
+
+echo "Sleep Started for Next Reseller_ID RUN"
+sleep 3
+
+done
+
+
+
+
