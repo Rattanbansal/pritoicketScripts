8efe9a783a3913b778e29cf31557da194b6d8ebb
Aadikar <rattan.intersoft@gmail.com>
2025-01-23 13:39:03 +0530

    new changes

--- EE_Orders/GetDataFromnBigquery.sh
+++ EE_Orders/GetDataFromnBigquery.sh
@@ -108,7 +108,7 @@ mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -e
 #     reseller_id INT(11)
 # );
 
-# select 
+# select  to check how many orders we have not any setting provided by client in google sheet name EE_Overview
 
 # select * from (select *,(case when qr_reseller_id = '541' and dt_hotel_id is null then 'No Setting' when qr_reseller_id != '541' and p_reseller_id is null then 'No Setting' else '' end) as status  from (SELECT ev.*,dt.ticket_id as dt_ticket_id, dt.hotel_id as dt_hotel_id, dt.commission as dt_commission,dt.cod_id as dt_cod_id,dt.sub_catalog_id as dt_sub_catalog_id,qr.reseller_id as qr_reseller_id,p.ticket_id as p_ticket_id,p.reseller_id as p_reseller_id,p.commission as p_commission FROM rattan.evanorders ev left join (select * from distributors UNION all select * from distributors1) as dt on ev.hotel_id = dt.hotel_id and ev.ticketId = dt.ticket_id left join priopassdb.qr_codes qr on qr.cod_id = ev.hotel_id left join pricelist p on p.reseller_id = qr.reseller_id and p.ticket_id = ev.ticketId) as main)  as orders where status ='No Setting'
  
--- EE_Orders/scriptMismatchZeroSum.sh
+++ EE_Orders/scriptMismatchZeroSum.sh
@@ -4,9 +4,13 @@ set -e  # Exit immediately if any command exits with a non-zero status
 TIMEOUT_PERIOD=25
 
 
-DB_HOST='10.10.10.19'
-DB_USER='pip'
-DB_PASS='pip2024##'
+# DB_HOST='10.10.10.19'
+# DB_USER='pip'
+# DB_PASS='pip2024##'
+source ~/vault/vault_fetch_creds.sh
+
+# Fetch credentials for 20Server
+fetch_db_credentials "19ServerNoVPN_db-creds"
 DB_NAME='rattan'
 BATCH_SIZE=100
 
@@ -22,16 +26,18 @@ mysqlUser=pipeuser
 mysqlPassword=d4fb46eccNRAL
 mysqlDatabase="prioprodrds"
 
+rm -f ZeroSumissue.csv
 
-timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" -D"$DB_NAME" -sN -e "update $tableName set channel_id = '0';select ROW_COUNT();" || exit 1  # channel_id used as status because not needed this column
+
+timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -sN -e "update $tableName set channel_id = '0';select ROW_COUNT();" || exit 1  # channel_id used as status because not needed this column
 
 # Get all unique ticket_ids
-ticket_ids=$(timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" -D"$DB_NAME" -sN -e "SELECT DISTINCT(ticketId) FROM $tableName where channel_id = '0'") || exit 1   # channel_id used in place of status
+ticket_ids=$(timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -sN -e "SELECT DISTINCT(ticketId) FROM $tableName where channel_id = '0'") || exit 1   # channel_id used in place of status
 
 # Loop through each ticket_id
 for ticket_id in $ticket_ids; do
     # Get all vt_group_no for the current ticket_id
-    vt_group_numbers=$(timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" -D"$DB_NAME" -sN -e "SELECT distinct vt_group_no FROM $tableName WHERE ticketId = '$ticket_id' and channel_id = '0'") || exit 1
+    vt_group_numbers=$(timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -sN -e "SELECT distinct vt_group_no FROM $tableName WHERE ticketId = '$ticket_id' and channel_id = '0'") || exit 1
     
     # Convert the vt_group_numbers into an array
     vt_group_array=($vt_group_numbers)
@@ -59,13 +65,13 @@ for ticket_id in $ticket_ids; do
         echo "Processing batch of size $batch_size for Ticket ID: $ticket_id ($current_progress / $total_vt_groups processed)" >> log.txt
         
                     
-        MISMATCHFInal="select * from (SELECT vt.vt_group_no, CONCAT(vt.transaction_id, 'R') AS transaction_id, sum(case when vt.row_type = '1' then vt.partner_net_price else 0 end) as salePrice, sum(case when vt.row_type in ('2','3','4','17') then vt.partner_net_price else 0 end) as otherPrice FROM visitor_tickets vt JOIN( SELECT vt_group_no, transaction_id, row_type, max(case when row_type = '1' then partner_net_price else 0 end) as salePrice, MAX(VERSION) AS VERSION FROM visitor_tickets WHERE ticketId = '$ticket_id' and vt_group_no IN($batch_str) AND col2 != '2' and transaction_type_name not like '%Reprice Surcharge%' and transaction_type_name not like '%Reprice Discount%' and row_type in ('1','2','3','4','17') GROUP BY vt_group_no, transaction_id ) AS maxversion ON vt.vt_group_no = maxversion.vt_group_no AND vt.transaction_id = maxversion.transaction_id and vt.col2 != '2' AND ABS(vt.version - maxversion.version) = '0' group by vt.vt_group_no,vt.transaction_id) as final where ABS(salePrice-otherPrice)>0.03;"
+        MISMATCHFInal="select * from (SELECT vt.vt_group_no, CONCAT(vt.transaction_id, 'R') AS transaction_id, sum(case when vt.row_type = '1' then vt.partner_net_price else 0 end) as salePrice, sum(case when vt.row_type in ('2','3','4','17') then vt.partner_net_price else 0 end) as otherPrice, hotel_id, ticketId, ticketpriceschedule_id, channel_id, reseller_id FROM visitor_tickets vt JOIN( SELECT vt_group_no, transaction_id, row_type, max(case when row_type = '1' then partner_net_price else 0 end) as salePrice, MAX(VERSION) AS VERSION FROM visitor_tickets WHERE ticketId = '$ticket_id' and vt_group_no IN($batch_str) AND col2 != '2' and transaction_type_name not like '%Reprice Surcharge%' and transaction_type_name not like '%Reprice Discount%' and action_performed like '%EECommission_Jan' and row_type in ('1','2','3','4','17') GROUP BY vt_group_no, transaction_id ) AS maxversion ON vt.vt_group_no = maxversion.vt_group_no AND vt.transaction_id = maxversion.transaction_id and vt.col2 != '2' AND ABS(vt.version - maxversion.version) = '0' group by vt.vt_group_no,vt.transaction_id) as final where ABS(salePrice-otherPrice)>0.03;"
 
 
         # sleep 3
-        timeout $TIMEOUT_PERIOD time mysql -h"$mysqlHost" -u"$mysqlUser" -p"$mysqlPassword" -D"$mysqlDatabase" -sN -e "$MISMATCHFInal" >> ZeroSumissue.csv || exit 1
+        timeout $TIMEOUT_PERIOD time mysql -h"$mysqlHost" -u"$mysqlUser" -p"$mysqlPassword" -D"$mysqlDatabase"  -e "$MISMATCHFInal" >> ZeroSumissue.csv || exit 1
 
-        timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" -D"$DB_NAME" -sN -e "update $tableName set channel_id = '3' where vt_group_no in ($batch_str)" || exit 1  # channel_id used as status because not needed this column
+        timeout $TIMEOUT_PERIOD time mysql -h"$DB_HOST" -u"$DB_USER" --port=$DB_PORT -p"$DB_PASSWORD" -D"$DB_NAME" -sN -e "update $tableName set channel_id = '3' where vt_group_no in ($batch_str)" || exit 1  # channel_id used as status because not needed this column
 
         
         echo "Sleep Started to Run next VGNS"
