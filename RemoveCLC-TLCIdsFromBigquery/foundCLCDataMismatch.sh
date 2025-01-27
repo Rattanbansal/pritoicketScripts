ec9b0a5f9422b45401deefa0bfad6766466eccc0
Aadikar <rattan.intersoft@gmail.com>
2025-01-27 15:49:57 +0530

    new changes

--- RemoveCLC-TLCIdsFromBigquery/foundCLCDataMismatch.sh
+++ RemoveCLC-TLCIdsFromBigquery/foundCLCDataMismatch.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+
+# ./foundCLCDataMismatch.sh <upload> <bqtablename> <mysqltable> <primarykey> <nosofdays>
+
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
-
+bqtablename=$2
+mysqltablename=$3
+primarykey=$4
+nosofdays=$5
 
 rm -rf *.json
 
-gcloud config set project prioticket-reporting
-bq query --use_legacy_sql=False --format=prettyjson \
-"delete FROM prioticket-reporting.prio_test.channel_level_mismatch where 1=1"
 
 maxLoopLimit=5000
 limit=250000
@@ -23,13 +26,17 @@ Uploaddata=$1
 
 if [[ "$Uploaddata" == "upload" ]]; then
 
+    gcloud config set project prioticket-reporting
+    bq query --use_legacy_sql=False --format=prettyjson \
+    "delete FROM prioticket-reporting.prio_test.$bqtablename where 1=1"
+
     echo "<<<<<<<<<<<<-------------Loop for Hotel ID: Started----------------->>>>>>>"
 
     while [ $i -le $maxLoopLimit ]
 
     do
 
-    data=$(mysql -h $DBHOST --user=$DBUSER --password=$DBPWD $DBDATABASE -N -e "select count(*) from (select * from channel_level_commission where deleted = '0' limit $offset, $limit) as base;")
+    data=$(mysql -h $DBHOST --user=$DBUSER --password=$DBPWD $DBDATABASE -N -e "select count(*) from (select * from $mysqltablename where deleted = '0' limit $offset, $limit) as base;")
     
     
     echo $data
@@ -46,7 +53,7 @@ if [[ "$Uploaddata" == "upload" ]]; then
     echo "Record from database Found using user id: $user and offset : $offset and limit : $limit" #>> inserted_data.txt
 
 
-    echo "select channel_level_commission_id, ticket_id, ticketpriceschedule_id, last_modified_at from channel_level_commission where deleted = '0' limit $offset, $limit" | time mysqlsh --sql --json --uri $DBUSER@$DBHOST -p$DBPWD --database=$DBDATABASE >> "$offset"_primarypt.json
+    echo "select $primarykey, ticket_id, ticketpriceschedule_id, last_modified_at from $mysqltablename where deleted = '0' limit $offset, $limit" | time mysqlsh --sql --json --uri $DBUSER@$DBHOST -p$DBPWD --database=$DBDATABASE >> "$offset"_primarypt.json
 
     jq 'select(.warning | not)' "$offset"_primarypt.json >> "$offset"_primarypt1.json
 
@@ -70,23 +77,39 @@ if [[ "$Uploaddata" == "upload" ]]; then
     do 
     echo $FILE; 
 
-    bq load --source_format=NEWLINE_DELIMITED_JSON  prio_test.channel_level_mismatch $FILE
+    bq load --source_format=NEWLINE_DELIMITED_JSON  prio_test.$bqtablename $FILE
 
 
     done
-
-
     sleep 15;
-
 fi
 
 # gcloud config set project prioticket-reporting-test
-bq query --use_legacy_sql=False --format=prettyjson \
-"select count(*) FROM prioticket-reporting.prio_test.channel_level_mismatch"
+bigquerycount=$(bq query --use_legacy_sql=False --format=prettyjson \
+"select count(*) as pcs FROM prioticket-reporting.prio_test.$bqtablename" | jq -r '.[] | .pcs')
 
-bq query --use_legacy_sql=False --max_rows=1000000 --format=prettyjson \
-"with base as (SELECT tlc.*, clc.channel_level_commission_id as id FROM prioticket-reporting.prio_test.channel_level_mismatch tlc left join prioticket-reporting.prio_olap.channel_level_commission clc on tlc.channel_level_commission_id = clc.channel_level_commission_id and tlc.ticket_id = clc.ticket_id and tlc.ticketpriceschedule_id = clc.ticketpriceschedule_id and tlc.last_modified_at = clc.last_modified_at) select * from base where id is NULL" > final_mismatch.csv
+mysqlcount=$(mysql -h 10.10.10.19 -u pip -p'pip2024##' priopassdb -sN -e "select count(*) as pcs from $mysqltablename where deleted = '0'")
 
-mysql -h 10.10.10.19 -u pip -p'pip2024##' priopassdb -e "select count(*) from channel_level_commission where deleted = '0'"
+if [[ "$bigquerycount" == "$mysqlcount" ]]; then
+
+    echo "bigquery Count:$bigquerycount"
+    echo "Mysql Count:$mysqlcount"
+
+    echo "Count Matched Proceed with next step"
+
+    bq query --use_legacy_sql=False --max_rows=10000000 --format=prettyjson \
+    "select count(distinct($primarykey)) as ids from prio_olap.$mysqltablename where $primarykey not in (select distinct $primarykey from prio_test.$bqtablename) and last_modified_at < TIMESTAMP(CONCAT(CAST(DATE(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL $nosofdays DAY)) AS STRING), ' 00:00:00'))" > final_mismatch.csv
+
+    # bq query --use_legacy_sql=False --max_rows=10000000 --format=prettyjson \
+    # "update prio_olap.$mysqltablename set hgs_postpaid_commission_percentage = 44.44 where $primarykey in (select distinct($primarykey) from prio_olap.$mysqltablename where $primarykey not in (select distinct $primarykey from prio_test.$bqtablename) and last_modified_at < TIMESTAMP(CONCAT(CAST(DATE(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL $nosofdays DAY)) AS STRING), ' 00:00:00')))"
+
+    bq query --use_legacy_sql=False --max_rows=10000000 --format=prettyjson \
+    "select distinct($primarykey) from prio_olap.$mysqltablename where $primarykey not in (select distinct $primarykey from prio_test.$bqtablename) and hgs_postpaid_commission_percentage != 44.44 and last_modified_at < TIMESTAMP(CONCAT(CAST(DATE(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL $nosofdays DAY)) AS STRING), ' 00:00:00'))" > final_mismatch.json
+else
+    echo "Count not matched"
+    echo "bigquery Count:$bigquerycount"
+    echo "Mysql Count:$mysqlcount"
+    exit 1
+fi
 
 rm -rf *.json
